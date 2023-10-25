b568cea2d058f534d7826bd5878d70ca75f495a2
JMTamayo <jmtamayog23@gmail.com>
2023-10-24 19:44:59 -0500

    Updating .lib file to include proyect changes: ClientCredentials and stream information.

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@
 //!
 //! With redsumer-rs you can:
 //! - **Produce** stream messages in specific *database* and *stream name*.
-//! - **Consume** stream messages from specific *stream name* and *consumer group*, setting config parameters like *min idle time*, *fisrt id*, *max number of messages to consume* and more. Also you can set message as consumed for specific *consumer group*.
+//! - **Consume** stream messages from specific *stream name* and *consumer group*, setting config parameters like *min idle time*, *first id*, *max number of messages to consume* and more. Also you can set message as consumed for specific *consumer group*.
 //!
 //! To use redsumer-rs from github repository, set the dependency in Cargo.toml file as follows:
 //!
@@ -15,7 +15,7 @@
 //!
 //!  ```ini
 //! [dependencies]
-//! redsumer-rs = {git = "https://github.com/enerBit/redsumer-rs", version = "0.2.2"}
+//! redsumer-rs = {git = "https://github.com/enerBit/redsumer-rs", version = "*"}
 //! ```
 //!
 //! For more dependency options from git, check section [3.0 Cargo Reference](https://doc.rust-lang.org/cargo/reference/index.html) from `The Cargo Book`.
@@ -24,14 +24,31 @@
 mod redsumer;
 mod tests;
 
-pub use redis::{streams::StreamId, FromRedisValue, RedisError, ToRedisArgs};
+/// Structs and traits from crate [redis](https://docs.rs/redis/0.23.3/redis) and its module [redis::streams](https://docs.rs/redis/0.23.3/redis/streams). Traits to get streams information.
+pub mod streams {
+    pub use redis::streams::{
+        StreamId, StreamInfoConsumer, StreamInfoConsumersReply, StreamInfoGroup,
+        StreamInfoGroupsReply, StreamInfoStreamReply,
+    };
+    pub use redis::{FromRedisValue, RedisError, ToRedisArgs};
 
-/// Redis Streams Producer
+    pub mod info {
+        // Utilities to get stream, group and consumers information.
+        pub use crate::redsumer::stream_information::{StreamConsumersInfo, StreamInfo};
+    }
+}
+
+/// Redis client configuration parameters
+pub mod client {
+    pub use crate::redsumer::client::ClientCredentials;
+}
+
+/// Redis streams producer handler
 pub mod producer {
     pub use crate::redsumer::producer::*;
 }
 
-/// Redis Streams Consumer
+/// Redis streams consumer handler
 pub mod consumer {
     pub use crate::redsumer::consumer::*;
 }
